d0b301b72bec13bc2b162f0783aafe4a15bc2a62
mmohareb <mina.mohareb@docusign.com>
2025-01-03 12:13:37 +1100

    Update Swift package checksum
    Signed-off-by: mmohareb <mina.mohareb@docusign.com>

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .binaryTarget(name: "DocuSignSDK", url: "https://docucdn-a.akamaihd.net/prod/docusigniossdk/3.5.0/DocuSignSDK.zip", checksum: "24e7dfb257b36986ff016333d68d9ad9387415f2623367522f55f87e8e188884"),
-        .binaryTarget(name: "DocuSignAPI", url: "https://docucdn-a.akamaihd.net/prod/docusigniossdk/3.5.0/DocuSignAPI.zip", checksum: "894699791e49b43de165aac62874e27f2467392dcb210ef26c8a78191b8a7534"),
+        .binaryTarget(name: "DocuSignSDK", url: "https://docucdn-a.akamaihd.net/prod/docusigniossdk/3.5.0/DocuSignSDK.zip", checksum: "200a4c1245afe669ca04913e8d8c3494a64e9e9445302067d74fe9fb44a8a2eb"),
+        .binaryTarget(name: "DocuSignAPI", url: "https://docucdn-a.akamaihd.net/prod/docusigniossdk/3.5.0/DocuSignAPI.zip", checksum: "e95bf8f205de1f6e68169207a995a29fd3e57c96a16daccd0de93558e19047cb"),
         ]
 )
