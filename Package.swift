aa6de69e33830efb9d62c3b3757583f8bbedf5f1
mmohareb <mina.mohareb@docusign.com>
2025-04-11 10:17:12 -0700

    remove extra char in checksum
    Signed-off-by: mmohareb <mina.mohareb@docusign.com>

--- Package.swift
+++ Package.swift
@@ -11,8 +11,8 @@ let package = Package(
         .library(name: "DocusignNative", targets: ["DocusignNative"]),
         ],
     targets: [
-        .binaryTarget(name: "DocuSignSDK", url: "https://docucdn-a.akamaihd.net/prod/docusigniossdk/4.0.1/DocuSignSDK.zip", checksum: "35m0834d3764c18d9538288ac379778172451b1e13646608d47ffd8f6a54f91094e"),
-        .binaryTarget(name: "DocuSignAPI", url: "https://docucdn-a.akamaihd.net/prod/docusigniossdk/4.0.1/DocuSignAPI.zip", checksum: "35m839c637dba1e11961b9c92073364505332019aa742711554685349c84cd1dd6d"),
-        .binaryTarget(name: "DocusignNative", url: "https://docucdn-a.akamaihd.net/prod/docusigniossdk/4.0.1/DocusignNative.zip", checksum: "35m1ae049dfc42973d4ae3de498b329dad8b5dd9987d0e79ef1ca1936de90acebb8"),
+        .binaryTarget(name: "DocuSignSDK", url: "https://docucdn-a.akamaihd.net/prod/docusigniossdk/4.0.1/DocuSignSDK.zip", checksum: "0834d3764c18d9538288ac379778172451b1e13646608d47ffd8f6a54f91094e"),
+        .binaryTarget(name: "DocuSignAPI", url: "https://docucdn-a.akamaihd.net/prod/docusigniossdk/4.0.1/DocuSignAPI.zip", checksum: "839c637dba1e11961b9c92073364505332019aa742711554685349c84cd1dd6d"),
+        .binaryTarget(name: "DocusignNative", url: "https://docucdn-a.akamaihd.net/prod/docusigniossdk/4.0.1/DocusignNative.zip", checksum: "1ae049dfc42973d4ae3de498b329dad8b5dd9987d0e79ef1ca1936de90acebb8"),
         ]
 )
