737b09d71f4571df2d283197aad2886d31b0f425
Ashok Meena <ashok.meena@gmail.com>
2026-04-02 13:22:49 -0700

    v4.1.1 - RC2
    * Update for issues/186: During low-quality network conditions, the sync routine ended up sending false-negative failed notification instead of succeeded notification. Fortified the logic around it and additional checks have been added.
    * DSMEnvelopesManager syncEnvelope(withId:) sends a specific envelope to sync with server and also bypass the max-sync attempt block to retry syncing a failing envelope.
    
    * Sync Notifications have a network reachability info added with "networkReachability" key.

--- Package.swift
+++ Package.swift
@@ -11,8 +11,8 @@ let package = Package(
         .library(name: "DocusignNative", targets: ["DocusignNative"]),
         ],
     targets: [
-        .binaryTarget(name: "DocuSignSDK", url: "https://docutest-a.akamaihd.net/test/docusigniossdk/4.1.1/DocuSignSDK.zip", checksum: "8c5fa5c0ad84731f764e88acc0a4a7235f7890acfefe57908ea4a60a741fe54f"),
-        .binaryTarget(name: "DocuSignAPI", url: "https://docutest-a.akamaihd.net/test/docusigniossdk/4.1.1/DocuSignAPI.zip", checksum: "681beb85166fc567f80f715674c7f6920110f07860e30ccd1a1675e858e83209"),
-        .binaryTarget(name: "DocusignNative", url: "https://docutest-a.akamaihd.net/test/docusigniossdk/4.1.1/DocusignNative.zip", checksum: "378ca031faa256f39f82e7a8c398e698e06d76b5a8db6c57503842bea2a11526"),
+        .binaryTarget(name: "DocuSignSDK", url: "https://docutest-a.akamaihd.net/test/docusigniossdk/4.1.1/DocuSignSDK.zip", checksum: "2180ce9772226e13c282046c3f005987eb1fc131c602e78380f82261d672e76a"),
+        .binaryTarget(name: "DocuSignAPI", url: "https://docutest-a.akamaihd.net/test/docusigniossdk/4.1.1/DocuSignAPI.zip", checksum: "39d3f61aef12ef8fa210518c743652f430b3ce00c8a23c9b29e56a4796677471"),
+        .binaryTarget(name: "DocusignNative", url: "https://docutest-a.akamaihd.net/test/docusigniossdk/4.1.1/DocusignNative.zip", checksum: "af597074b5554921d90510c8dcbe01bf85641834a3d62fdae0ebe1103a5ac4f3"),
         ]
 )
