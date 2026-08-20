d7ec0b12570a5090b60d0f2c182c429fcf5f1a8c
Ashok Meena <ashokds@users.noreply.github.com>
2026-08-20 13:33:02 -0700

    Update Package.swift Hashes
    Updated hash for the package.
    
    Signed-off-by: Ashok Meena <ashokds@users.noreply.github.com>

--- Package.swift
+++ Package.swift
@@ -11,8 +11,8 @@ let package = Package(
         .library(name: "DocusignNative", targets: ["DocusignNative"]),
         ],
     targets: [
-        .binaryTarget(name: "DocuSignSDK", url: "https://docucdn-a.akamaihd.net/prod/docusigniossdk/4.3.0/DocuSignSDK.zip", checksum: "25724bf87023da6b0c60741f1de70a95dc968d9a274f8d6c4789731b87a9969a"),
-        .binaryTarget(name: "DocuSignAPI", url: "https://docucdn-a.akamaihd.net/prod/docusigniossdk/4.3.0/DocuSignAPI.zip", checksum: "091df294226237b026a141284a4416cc37f3d2b646692c26606df40219056999"),
-        .binaryTarget(name: "DocusignNative", url: "https://docucdn-a.akamaihd.net/prod/docusigniossdk/4.3.0/DocusignNative.zip", checksum: "40a322d31340e3bc4344c9e630b3a1c4d84fec3af81c6a17819e2aafd66d0c77"),
+        .binaryTarget(name: "DocuSignSDK", url: "https://docucdn-a.akamaihd.net/prod/docusigniossdk/4.3.0/DocuSignSDK.zip", checksum: "cee5facc466f795b6bccb03ce875cfec5ce41930032cedbb2604572a9160d133"),
+        .binaryTarget(name: "DocuSignAPI", url: "https://docucdn-a.akamaihd.net/prod/docusigniossdk/4.3.0/DocuSignAPI.zip", checksum: "07c6cd3364009eabdaeb2bd76b43bae524adc95ecd329338950e9628e866bfd1"),
+        .binaryTarget(name: "DocusignNative", url: "https://docucdn-a.akamaihd.net/prod/docusigniossdk/4.3.0/DocusignNative.zip", checksum: "bf23a2830d948cc0251ff7d28cb820f7673baf0957b03a6e1d88bbe92b2af855"),
         ]
 )
